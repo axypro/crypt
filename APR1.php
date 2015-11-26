fe2f54b5e0c6254299f878c2e4fb9e877116e6c8
Oleg Grigoriev <go.vasac@gmail.com>
2015-11-26 17:59:04 +0300

    APR1::createSubHash(): Creates a hash for a string and a salt

--- APR1.php
+++ APR1.php
@@ -12,7 +12,11 @@ namespace axy\crypt;
 class APR1
 {
     const ALPHABET = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const BASE64_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
     const SALT_LENGTH = 8;
+    const PREFIX = '$apr1$';
+    const COUNT_STEPS = 1000;
+    const HASH_COUNT_STEPS = 5;
 
     /**
      * Creates a random salt
@@ -29,5 +33,85 @@ class APR1
             $salt[] = substr($alphabet, $index, 1);
         }
         return implode('', $salt);
+    }
+
+    /**
+     * Creates a hash for a string and a salt
+     *
+     * @param string $string
+     * @param string $salt
+     * @return string
+     */
+    public static function createSubHash($string, $salt)
+    {
+        $context = self::createContext($string, $salt);
+        $null = chr(0);
+        $hash = '';
+        for ($i = 0; $i < self::HASH_COUNT_STEPS; $i++) {
+            $k = $i + 6;
+            $j = $i + 12;
+            if ($j === 16) {
+                $j = 5;
+            }
+            $hash = $context[$i].$context[$k].$context[$j].$hash;
+        }
+        $hash = $null.$null.$context[11].$hash;
+        $hash = base64_encode($hash);
+        $hash = substr($hash, 2);
+        $hash = strrev($hash);
+        $hash = strtr($hash, self::BASE64_ALPHABET, self::ALPHABET);
+        return $hash;
+    }
+
+    /**
+     * @param string $string
+     * @param string $salt
+     * @return string
+     */
+    private static function createContext($string, $salt)
+    {
+        $len = strlen($string);
+        $null = chr(0);
+        $context = $string.self::PREFIX.$salt;
+        $binary = pack('H32', md5($string.$salt.$string));
+        for ($i = $len; $i > 0; $i -= 16) {
+            $context .= substr($binary, 0, min(16, $i));
+        }
+        for ($i = $len; $i > 0; $i >>= 1) {
+            $context .= ($i & 1) ? $null : $string[0];
+        }
+        $context = pack('H32', md5($context));
+        return self::iterateContext($string, $salt, $context);
+    }
+
+    /**
+     * @param string $string
+     * @param string $salt
+     * @param string $context
+     * @return string
+     */
+    private static function iterateContext($string, $salt, $context)
+    {
+        for ($i = 0; $i < self::COUNT_STEPS; $i++) {
+            $value = [];
+            if ($i % 2) {
+                $value[] = $string;
+            } else {
+                $value[] = $context;
+            }
+            if ($i % 3) {
+                $value[] = $salt;
+            }
+            if ($i % 7) {
+                $value[] = $string;
+            }
+            if ($i % 2) {
+                $value[] = $context;
+            } else {
+                $value[] = $string;
+            }
+            $context = pack('H32', md5(implode('', $value)));
+        }
+        return $context;
     }
 }
--- tests/APR1Test.php
+++ tests/APR1Test.php
@@ -23,4 +23,29 @@ class APR1Test extends \PHPUnit_Framework_TestCase
         $pattern = '~^[A-Za-z0-9\./]{8}$~is';
         $this->assertRegExp($pattern, $salt);
     }
+
+    /**
+     * covers ::createSubHash
+     * @dataProvider providerCreateSubHash
+     * @param string $string
+     * @param string $salt
+     * @param string $expected
+     */
+    public function testCreateSubHash($string, $salt, $expected)
+    {
+        $this->assertSame($expected, APR1::createSubHash($string, $salt));
+    }
+
+    /**
+     * @return string
+     */
+    public function providerCreateSubHash()
+    {
+        return [
+            ['one', 'IQ9IZAC8', 'wunOzzELYRnoT1g6Oj.ec0'],
+            ['one', 'kvpZmkx6', 'zHyJ71iVSkoJxw4jOXpAu0'],
+            ['two', 'CBIuqhN2', 'lYcwqwZkBBUKhshGYHA/P/'],
+            ['this-is-a-Password', 'tj9CTOta', 'EivyGJIIa8Gwb5QFP.9dz1'],
+        ];
+    }
 }
